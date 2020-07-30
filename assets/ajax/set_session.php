162edc132786d27a00a159ee5a6ef101d715b77e
mazen01010 <41298065+mazen01010@users.noreply.github.com>
2020-07-30 12:41:16 +0200

    update

--- assets/ajax/set_session.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-// Basic example of PHP script to handle with jQuery-Tabledit plug-in.
-// Note that is just an example. Should take precautions such as filtering the input data.
-
-if (!isset($_SERVER['HTTP_X_REQUESTED_WITH'])) die("Nothing to see here");
